d5c26e1bb288e9e5cc001f3b33a4366a37b36a0a
Facundo Rodriguez Sabia <facundorsabia@gmail.com>
2024-07-10 10:26:01 -0300

    Some updates

--- test.py
+++
@@ -1,9 +0,0 @@
-import google.generativeai as genai
-import os
-
-genai.configure(api_key=os.environ["AIzaSyCMlwNbVEoIuhqxm63jeTf4a44ckVc2_kI"])
-
-model = genai.GenerativeModel('gemini-1.5-flash')
-
-response = model.generate_content("Are you alive?")
-print(response.text)
