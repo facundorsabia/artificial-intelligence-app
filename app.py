237a45a5a7c220327b76c89739f09f56f4076993
Facundo Rodriguez Sabia <facundorsabia@gmail.com>
2024-07-11 21:13:16 -0300

    updates

--- app.py
+++ app.py
@@ -2,28 +2,24 @@
 ##Gemini IA library !pip install -q -U google-generativeai
 ##Python library user pip install ipython
 
-import pathlib
-import textwrap
+
+from dotenv import load_dotenv
+import os
 
 import google.generativeai as genai
 
-from IPython.display import display 
 from IPython.display import Markdown
 
 import streamlit as st
-import streamlit.components.v1 as components
+
+# Cargar las variables de entorno desde el archivo .env
+load_dotenv()
 
 #GEMINI API Configuration
 
-def to_markdown(text):
-    lines = text.strip().split('\n')
-    indented_lines = [f"> {line}" for line in lines]
-    indented_text = '\n'.join(indented_lines)
-    return Markdown(indented_text)
 
 
-
-GOOGLE_API_KEY= 'AIzaSyCMlwNbVEoIuhqxm63jeTf4a44ckVc2_kI'
+GOOGLE_API_KEY= os.getenv('GOOGLE_API_KEY')
 
 genai.configure(api_key=GOOGLE_API_KEY)
 
@@ -32,17 +28,25 @@ model = genai.GenerativeModel('gemini-1.0-pro')
 # APP Streamlit
 
 st.set_page_config(
-    page_title="Recomendador de Lecturas",
+    page_title="BiblioMente",
     layout="wide",
     initial_sidebar_state="expanded",
 )
 
-st.title('Recomendador de Lecturas')
+st.title('BiblioMente')
+st.subheader('Descubre nuevas lecturas adaptadas a tus gustos literarios')
+st.markdown("### Tu guía personalizada de libros, blogs y artículos en diversos géneros potenciada por Inteligencia Artificial")
 
 genero = st.selectbox('¿Qué género literario prefieres?', ['Fantasía', 'No Ficción', 'Filosofía', 'Poesía', 'Misterio', 'Terror', 'Romance', 'Ciencia Ficción', 'Histórico', 'Biográfico', 'Autoayuda', 'Política'])
 tipo = st.radio('¿Estás buscando libros, blogs o artículos?', ['Libros', 'Blogs', 'Artículos'])
 temas = st.text_input('¿Qué temas específicos te interesan actualmente?')
 
+
+def to_markdown(text):
+    lines = text.strip().split('\n')
+    indented_lines = [f"> {line}" for line in lines]
+    indented_text = '\n'.join(indented_lines)
+    return Markdown(indented_text)
 
 def obtener_recomendaciones(genero, tipo, temas):
     prompt = f"Quiero que seas un experto en literatura. Recomienda {tipo} sobre {temas} en el género {genero}. Además agrega una pequeña reflexión final acerca de {temas} en {genero}" 
